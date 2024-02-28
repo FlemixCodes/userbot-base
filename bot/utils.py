9025855c1df1dd55a39f088427016f9cd7a926ee
Pavel <pavelvodocnikov@gmail.com>
2024-02-28 20:59:37 +0500

    update utils

--- bot/utils.py
+++ bot/utils.py
@@ -14,12 +14,3 @@ def get_user_id_reply(message: Message) -> int | None:
     """Получить айди пользователя из пересланного сообщения"""
     if message.reply_message:
         return message.reply_message.from_id
-
-
-async def check_token_valid(api: API) -> bool:
-    """Проверить access_token на валидность через объект API"""
-    try:
-        await api.account.get_profile_info()
-        return True
-    except VKAPIError[5]:
-        return False
